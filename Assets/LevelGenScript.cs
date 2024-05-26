b2f5a742cae55effa8cf7b61c8c486f08abef7ab
Ziad-J05 <ziadjaafar05@gmail.com>
2024-05-26 18:32:14 -0400

    added method stubs and custom graph

--- Assets/LevelGenScript.cs
+++ Assets/LevelGenScript.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class LevelGenScript : MonoBehaviour
 {
+    // holds all possible types of dungeon rooms
+    public List<GameObject> rooms = new List<GameObject>();
+
+    // graphs that determine 
+    private List<Graph> layouts = new List<Graph>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +21,59 @@ public class LevelGenScript : MonoBehaviour
     {
         
     }
+
+    /// <summary>
+    /// Creates a new level in the scene based on a randomly-chosen layout graph
+    /// </summary>
+    private void GenerateLevel()
+    {
+
+    }
+
+    /// <summary>
+    /// Removes all rooms onscreen
+    /// </summary>
+    private void DestroyLevel()
+    {
+        // rooms will have a "room" tag, and this method will find all the objects
+        // with that tag and destroy them
+    }
 }
+
+/// <summary>
+/// Unweighted, directed graph class for storing dungeon layouts
+/// </summary>
+public class Graph
+{
+    private List<Vertex> verticies;
+    private int[,] adjMatrix;
+    private List<List<Vertex>> adjList;
+
+    /// <summary>
+    /// Create a new graph given a list of verticies and an adjacency matrix, where the
+    /// first vertex in the list is the starting room and the last is the ending room
+    /// </summary>
+    public Graph(List<Vertex> verticies, int[,] adjacencies)
+    {
+        CreateAdjList();
+    }
+
+    private void CreateAdjList()
+    {
+
+    }
+
+}
+
+/// <summary>
+/// Vertex for custom graph class
+/// </summary>
+public class Vertex
+{
+    private RoomType type;
+}
+
+/// <summary>
+/// Stores what each dungeon room does
+/// </summary>
+public enum RoomType { Start, End, Normal, Special }
