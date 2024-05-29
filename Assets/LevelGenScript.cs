2374ab03569b4395459e540b35cfd04d17cc9380
Ziad-J05 <ziadjaafar05@gmail.com>
2024-05-29 16:40:13 -0400

    added code to graph class

--- Assets/LevelGenScript.cs
+++ Assets/LevelGenScript.cs
@@ -13,7 +13,7 @@ public class LevelGenScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // initialize all possible layouts and room types
     }
 
     // Update is called once per frame
@@ -25,15 +25,18 @@ public class LevelGenScript : MonoBehaviour
     /// <summary>
     /// Creates a new level in the scene based on a randomly-chosen layout graph
     /// </summary>
-    private void GenerateLevel()
+    public void GenerateLevel()
     {
+        // choose a random layout
+        Graph layout = layouts[Random.Range(0, layouts.Count)];
+        // generate rooms in path order
 
     }
 
     /// <summary>
     /// Removes all rooms onscreen
     /// </summary>
-    private void DestroyLevel()
+    public void DestroyLevel()
     {
         // rooms will have a "room" tag, and this method will find all the objects
         // with that tag and destroy them
@@ -50,19 +53,48 @@ public class Graph
     private List<List<Vertex>> adjList;
 
     /// <summary>
+    /// List of all verticies in the graph
+    /// </summary>
+    public List<Vertex> Vertices
+    {
+        get { return verticies; }
+    }
+
+    /// <summary>
+    /// Adjacency list of all the verticies in the graph
+    /// </summary>
+    public List<List<Vertex>> AdjList
+    {
+        get { return adjList; }
+    }
+
+    /// <summary>
     /// Create a new graph given a list of verticies and an adjacency matrix, where the
     /// first vertex in the list is the starting room and the last is the ending room
     /// </summary>
     public Graph(List<Vertex> verticies, int[,] adjacencies)
     {
+        this.verticies = verticies;
+        this.adjMatrix = adjacencies;
         CreateAdjList();
     }
 
+    /// <summary>
+    /// Create an adjacency list using a list of verticies and an adjacency matrix
+    /// </summary>
     private void CreateAdjList()
     {
+        adjList = new List<List<Vertex>>();
+        for (int i = 0; i < adjMatrix.GetLength(0); i++)
+        {
+            adjList.Add(new List<Vertex>());
 
+            for (int j = 0; j < adjMatrix.GetLength(1); j++)
+            {
+                adjList[i].Add(verticies[j]);
+            }
+        }
     }
-
 }
 
 /// <summary>
