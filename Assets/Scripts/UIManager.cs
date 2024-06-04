fcc6bb0e55c34345ba3857b24e7583cbe5b214f3
Jeff <cytastorm@gmail.com>
2024-06-04 11:59:31 -0400

    added aether bar and it links up with the player's mana value

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,18 +6,27 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    [Header("GameObjec Components")]
     [SerializeField] private GameObject _healthBar;
+    [SerializeField] private GameObject _aetherBar;
     [SerializeField] private GameObject _player;
 
     private Slider _healthBarSlider;
+    private Slider _aetherBarSlider;
+    private PlayerStats _playerStats;
     void Start()
     {
+        _playerStats = _player.GetComponent<PlayerStats>();
         _healthBarSlider = _healthBar.GetComponent<Slider>();
+        _aetherBarSlider = _aetherBar.GetComponent<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _healthBarSlider.value = _player.GetComponent<PlayerStats>().PlayerHealth;
+        _healthBarSlider.value = _playerStats.PlayerHealth;
+        _healthBarSlider.maxValue = _playerStats.PlayerMaxHealth;
+        _aetherBarSlider.value = _playerStats.PlayerMana;
+        _aetherBarSlider.minValue = _playerStats.PlayerMaxMana;
     }
 }
