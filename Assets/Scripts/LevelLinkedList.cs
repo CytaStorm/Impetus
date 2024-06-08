0391a4ed586e36e01bcea967562cc62f06e6af67
Ziad-J05 <ziadjaafar05@gmail.com>
2024-06-07 20:40:54 -0400

    basically finished linked list and node classes

--- Assets/Scripts/LevelLinkedList.cs
+++ Assets/Scripts/LevelLinkedList.cs
@@ -1,18 +1,208 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using static UnityEditor.Progress;
 
+/// <summary>
+/// Linked list class for storing dungeon layouts
+/// </summary>
 public class LevelLinkedList : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private LevelNode head;
+    private LevelNode tail;
+    private int count;
+
+    /// <summary>
+    /// Number of nodes in the list
+    /// </summary>
+    public int Count {  get { return count; } }
+
+    /// <summary>
+    /// Create an empty linked list
+    /// </summary>
+    public LevelLinkedList()
     {
-        
+        count = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Remove all data from the list
+    /// </summary>
+    public void Clear()
     {
-        
+        head = null;
+        tail = null;
+        count = 0;
+    }
+
+    /// <summary>
+    /// Adds a node to the end of the list
+    /// </summary>
+    public void Add(GameObject room)
+    {
+        if (count == 0)
+        {
+            count++;
+            head = new LevelNode(null, room);
+            tail = head;
+        }
+        else
+        {
+            count++;
+            tail.Next = new LevelNode(tail, room);
+            tail = tail.Next;
+        }
+    }
+
+    /// <summary>
+    /// Insert a node at a given index
+    /// </summary>
+    public void Insert(GameObject room, int index) 
+    {
+        // Validate index
+        if ((index > count) || (index < 0))
+        {
+            throw new IndexOutOfRangeException(
+                $"Error: Cannot insert into invalid index {index}.");
+        }
+
+        // Add data to the end
+        if (index == count)
+        {
+            Add(room); // Takes care of empty lists
+        }
+        // Add data to the start
+        else if (index == 0)
+        {
+            head.Previous = new LevelNode(null, room);
+            head.Previous.Next = head;
+            head = head.Previous;
+            count++;
+        }
+        // Add data to the middle
+        else
+        {
+            LevelNode current = GetNodeAt(index);
+            LevelNode newNode = new LevelNode(current.Previous, room);
+            current.Previous.Next = newNode;
+            newNode.Next = current;
+            current.Previous = newNode;
+            count++;
+        }
+    }
+
+    /// <summary>
+    /// Remove a node at the given index
+    /// </summary>
+    public void RemoveAt(int index) 
+    {
+        // Validate index
+        if ((index >= count) || (index < 0))
+        {
+            throw new IndexOutOfRangeException(
+                $"Error: Cannot remove invalid index {index}.");
+        }
+
+        // Remove head
+        if (index == 0)
+        {
+
+            // Null tail if head was the only node
+            if (count == 1)
+            {
+                head = null;
+                tail = null;
+            }
+            else
+            {
+                head = head.Next;
+                head.Previous = null;
+            }
+            count--;
+        }
+        // Remove tail
+        else if (index == count - 1)
+        {
+            tail = tail.Previous;
+            tail.Next = null;
+            count--;
+        }
+        // Remove in the middle
+        else
+        {
+            // See what end of the list the index is closest to
+            LevelNode current = GetNodeAt(index);
+            current.Next.Previous = current.Previous;
+            current.Previous.Next = current.Next;
+            count--;
+        }
+    }
+
+    /// <summary>
+    /// Indexer for the linked list
+    /// </summary>
+    /// <param name="index">Index of data to access</param>
+    /// <returns>Room at specified index</returns>
+    public GameObject this[int index]
+    {
+        get
+        {
+            // Check if index is invalid in either direction
+            if ((index < 0) || (index >= count))
+            {
+                throw new IndexOutOfRangeException(
+                    $"Error: Cannot get data from invalid index {index}.");
+            }
+
+            // See what end of the list the index is closest to
+            LevelNode current = GetNodeAt(index);
+            return current.Room;
+        }
+
+        set
+        {
+            // Check if index is invalid in either direction
+            if ((index < 0) || (index >= count))
+            {
+                throw new IndexOutOfRangeException(
+                    $"Error: Cannot set data at invalid index {index}.");
+            }
+
+            // See what end of the list the index is closest to
+            LevelNode current = GetNodeAt(index);
+            current.Room = value;
+        }
+    }
+
+    /// <summary>
+    /// Gets a node using the most efficient traversal possible
+    /// </summary>
+    /// <param name="index">Index of the node to get</param>
+    /// <returns>A reference to the node at the given index</returns>
+    private LevelNode GetNodeAt(int index)
+    {
+        // See what end of the list the index is closest to
+        LevelNode current;
+        if (count / 2 < index)
+        {
+            // Loop through list from the end
+            current = tail;
+            for (int i = count - 1; i > index; i--)
+            {
+                current = current.Previous;
+            }
+        }
+        else
+        {
+            // Loop through list from the start
+            current = head;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+        }
+        return current;
     }
 }
---
+++ Assets/Scripts/LevelNode.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Node for doubly-linked list
+/// </summary>
+public class LevelNode : MonoBehaviour
+{
+    private GameObject room;
+    private Direction startDir;
+    private Direction endDir;
+    private LevelNode previous;
+    private LevelNode next;
+
+    /// <summary>
+    /// Returns the room at the current location in the list.
+    /// </summary>
+    public GameObject Room
+    {
+        get { return room; }
+        set { room = value; }
+    }
+    /// <summary>
+    /// The direction of the entrance door relative to the center of the room.
+    /// </summary>
+    public Direction StartDir
+    {
+        get { return startDir; }
+    }
+    /// <summary>
+    /// The direction of the exit door relative to the center of the room.
+    /// </summary>
+    public Direction EndDir
+    {
+        get { return endDir; }
+    }
+    /// <summary>
+    /// Returns the node before this one in the linked list.
+    /// If the node is a head this will return null.
+    /// </summary>
+    public LevelNode Previous
+    {
+        get { return previous; }
+        set { previous = value; }
+    }
+    /// <summary>
+    /// Returns the node in front of this one in the linked list.
+    /// If the node is a tail this will return null.
+    /// </summary>
+    public LevelNode Next
+    {
+        get { return next; }
+        set { next = value; }
+    }
+
+    /// <summary>
+    /// Create a new node
+    /// </summary>
+    public LevelNode(LevelNode previous, GameObject room) 
+    { 
+        this.previous = previous;
+        this.room = room;
+    }
+}
+
+public enum Direction { Up, Down, Left, Right };
