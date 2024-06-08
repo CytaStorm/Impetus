88a1ff1d7c10184b855da54a743d941a09f211e3
Ziad-J05 <ziadjaafar05@gmail.com>
2024-06-07 21:21:05 -0400

    added method stubs to levelGeneration script

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class LevelGeneration : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // all possible room layouts
+    public List<GameObject> rooms;
+    // current layout
+    public LevelLinkedList layout;
+    // index of current room the player is in
+    public int roomIndex;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// Create a new layout linked list with empty rooms
+    /// </summary>
+    private void GenerateLayout() { }
+
+    /// <summary>
+    /// If the rooms in the layout are empty, fill them with the correct preset
+    /// </summary>
+    private void UpdateRooms() { }
+
+    /// <summary>
+    /// Render the room the player is currently in based on the roomIndex
+    /// </summary>
+    public void RenderCurrentRoom() { }
+
+    public void ResetLayout()
+    {
+        layout = null;
+        roomIndex = 0;
     }
 }
