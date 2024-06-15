8aa10661280ab3f3078fbc76a57d197d1b452f39
AazeemVS <av7304@rit.edu>
2024-06-14 21:22:02 -0400

    Created PlayerStats class

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float Health { get; set; }
+    public float Aether { get; set; }
+    public float Flow { get; set; }
+}
