25e3252ed84c6bc49b504f9bfecd764414b1c576
AazeemVS <av7304@rit.edu>
2024-06-20 14:33:02 -0400

    Revert "added variables to big enemy script"
    This reverts commit e1a6ccd341cd89f152905517f52ec825d1f91072.

--- Assets/Scripts/Enemies/bigEnemyScript.cs
+++ Assets/Scripts/Enemies/bigEnemyScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class bigEnemyScript : MonoBehaviour
 {
 
+    //Variable Declarations
     Path path;
     int currentWaypoint;
     bool reachedEndOfPath;
@@ -12,64 +13,27 @@ public class bigEnemyScript : MonoBehaviour
     Rigidbody2D rb;
     GameObject target;
 
+
     //nextWaypointDistance represents the distance you CAN be from the target waypoint before
     // switching to the next waypoint. This helps curve the path and make it more natural.
-    const float nextWayPointDistance = .3f;
-    const float speed = 1;
-    
+    const float nextWayPointDistance = .5f;
+    const float speed = 3;
+
+    //Define objects
+    target = GameObject.FindWithTag("Player");
+        seeker = this.GetComponent<Seeker>();
+        rb = this.GetComponent<Rigidbody2D>();
+
 
     // Start is called before the first frame update
     void Start()
     {
-        //Define objects
-        target = GameObject.FindWithTag("Player");
-        seeker = this.GetComponent<Seeker>();
-        rb = this.GetComponent<Rigidbody2D>();
-
-        //Setup CalculatePath() to run every half second
-        InvokeRepeating("CalculatePath", 0f, .25f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //First make sure the path is created
-        if (path == null)
-        {
-            return;
-        }
-
-        //MOVE
-        //find direction of the next waypoint (vector2)
-        Vector2 direction = (path.vectorPath[currentWaypoint] - this.transform.position);
-
-        //Multiply direction by speed and move
-        rb.velocity = direction.normalized * speed;
-
-        //If the distance is small enough, switch to next waypoint
-        if (direction.magnitude <= nextWayPointDistance)
-        {
-            currentWaypoint++;
-        }
-
-        //Are we at the end of the path?
-        if (currentWaypoint >= path.vectorPath.Count)
-        {
-            reachedEndOfPath = true;
-        }
-    }
-
-    /// <summary>
-    /// Calculate a new path every second
-    /// </summary>
-    void CalculatePath()
-    {
-        //First make sure the seeker is done calculating the last path
-        if (seeker.IsDone())
-        {
-            path = seeker.StartPath(this.transform.position, target.transform.position);
-            reachedEndOfPath = false;
-            currentWaypoint = 0;
-        }
+        
     }
 }
