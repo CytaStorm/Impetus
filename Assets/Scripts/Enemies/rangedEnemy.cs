ebf27284dd0727f6399bb65a0600d7f96b03950e
AazeemVS <av7304@rit.edu>
2024-06-23 04:52:57 -0400

    Did basic logic for rangedEnemy
    Got some of the movement down, need to add attack and his run away mechanic

---
+++ Assets/Scripts/Enemies/rangedEnemy.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class Enemy2Script : MonoBehaviour
+{
+    // Variable Declarations
+    Path path;
+    int currentWaypoint;
+    bool reachedEndOfPath;
+    Seeker seeker;
+    Rigidbody2D rb;
+    GameObject target;
+    public LayerMask obstructionMask;
+
+    // Get the universal enemy script from our enemy
+    EnemyScript enemyScript;
+
+    // nextWaypointDistance represents the distance you CAN be from the target waypoint before
+    // switching to the next waypoint. This helps curve the path and make it more natural.
+    const float nextWayPointDistance = .5f;
+    const float speed = 5f;
+    public float detectionRange = 10f;
+    public float evadeRange = 3f;
+    public float approachRange = 5f;
+
+    // Weakness variable
+    //Depending on if they use enums for weakness or not
+    //If they dont use enums we need to change the weakness logic
+    //public AttackType Weakness = AttackType.Piercing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Define objects
+        target = GameObject.FindWithTag("Player");
+        seeker = this.GetComponent<Seeker>();
+        rb = this.GetComponent<Rigidbody2D>();
+        enemyScript = this.GetComponent<EnemyScript>();
+
+        // SPECIFIC TO MEDIUM ENEMY
+        enemyScript.EnemyHealth = 300;
+        enemyScript.AetherIncrease = 20;
+        enemyScript.EnemyDamage = 15;
+        enemyScript.FlowWorth = 100;
+
+        // Setup CalculatePath() to run every half second
+        InvokeRepeating("CalculatePath", 0f, .25f);
+    }
+
+    void Update()
+    {
+        // First make sure the path is created
+        if (path == null)
+        {
+            return;
+        }
+
+        // Calculate distance to player
+        float distanceToPlayer = Vector2.Distance(this.transform.position, target.transform.position);
+
+        // Check if player is within detection range and there are no obstructions
+        if (distanceToPlayer <= detectionRange && !Physics2D.Linecast(transform.position, target.transform.position, obstructionMask))
+        {
+            if (distanceToPlayer <= evadeRange)
+            {
+                // Evade player
+                Vector2 direction = (this.transform.position - target.transform.position).normalized;
+                rb.velocity = direction * speed;
+            }
+            else if (distanceToPlayer <= approachRange)
+            {
+                // Stop moving when in approach range
+                rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                // Move towards player
+                MoveTowardsPlayer();
+            }
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    void MoveTowardsPlayer()
+    {
+        // Move towards the next waypoint
+        Vector2 direction = (path.vectorPath[currentWaypoint] - this.transform.position);
+        rb.velocity = direction.normalized * speed;
+
+        // If the distance is small enough, switch to the next waypoint
+        if (direction.magnitude <= nextWayPointDistance)
+        {
+            currentWaypoint++;
+        }
+
+        // Are we at the end of the path?
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+        }
+    }
+
+    /// <summary>
+    /// Calculate a new path (this is done every half second or second or so)
+    /// </summary>
+    void CalculatePath()
+    {
+        // First make sure the seeker is done calculating the last path
+        if (seeker.IsDone())
+        {
+            path = seeker.StartPath(this.transform.position, target.transform.position);
+            reachedEndOfPath = false;
+            currentWaypoint = 0;
+        }
+    }
+
+    /// <summary>
+    /// Draw the detection and evade radius when selected
+    /// </summary>
+    void OnDrawGizmosSelected()
+    {
+        // Draw the detection range
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(this.transform.position, detectionRange);
+
+        // Draw the approach range
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, approachRange);
+
+        // Draw the evade range
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(this.transform.position, evadeRange);
+    }
+}
