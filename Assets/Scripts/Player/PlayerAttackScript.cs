ae00a0645df02c03128c1f18cb9fdfcb4baaf03e
Jeff <cytastorm@gmail.com>
2024-07-18 19:38:33 -0400

    added attack cancelling

--- Assets/Scripts/Player/PlayerAttackScript.cs
+++ Assets/Scripts/Player/PlayerAttackScript.cs
@@ -7,134 +7,120 @@ using UnityEngine.InputSystem;
 public class PlayerAttackScript : MonoBehaviour
 {
 	#region GameObject Components
-	[Header("GameObject Components")]
 	[SerializeField] private GameObject _sword;
 	private Animator _swordAnimator;
 	private SpriteRenderer _swordSpriteRenderer;
+	[SerializeField] private AnimationClip _swingAnimationClip;
+	[SerializeField] private AnimationClip _thrustAnimationClip;
+	[SerializeField] private AnimationClip _slamAnimationClip;
+	[Space(20)]
 	#endregion
 
 	#region Attack Properties
-	[Header("Attack Properties")]
-	//Attack duration
-	[SerializeField] private float _attackDuration = 0.5f;
-	private float _currentAttackTime;
-	private bool _attacking;
-	private float _attackCompletion
-	{
-		get { return _currentAttackTime / _attackDuration; }
-	}
-	private Vector2 _mostRecentAttackDirection;
+	//Attack durations
+	[Range(0, 2)] [SerializeField] private float _swingAttackDuration;
+	[Range(0, 2)] [SerializeField] private float _thrustAttackDuration;
+	[Range(0, 2)] [SerializeField] private float _slamAttackDuration;
 
-	//Attack Cooldown
+	//Cooldowns
 	[SerializeField] private float _attackCooldownDuration = 2f;
-	private float _currentAttackCooldownTime;
-	private bool _attackReady
-	{
-		get { return _currentAttackCooldownTime == 0f; }
-	}
-	#endregion
 
-	#region Sword Drawing
-	[SerializeField][Range(0f, 360f)] private int _slashArc;
-	private Vector3 _slashArcOffset
-	{
-		get { return new Vector3(0, 0, _slashArc / 2); }
-	}
+	private bool _swingReady = true;
+	private bool _thrustReady = true;
+	private bool _slamReady = true;
 
 	#endregion
 
 	// Start is called before the first frame update
-	void Start()
+	private void Start()
 	{
 		_swordAnimator = _sword.GetComponent<Animator>();
 		_swordSpriteRenderer = _sword.GetComponent<SpriteRenderer>();
+
+		SetAttackSpeeds();
 	}
+
 
 	// Update is called once per frame
 	void Update()
 	{
-		#region Sword update
-		//Tick attack cooldown if possible and not attacking.
-		if (!_attacking)
-		{
-			if (!_attackReady)
-			{
-				_currentAttackCooldownTime =
-					Mathf.Clamp(_currentAttackCooldownTime - Time.deltaTime, 0f, _attackCooldownDuration);
-			}
-		}
-		//Is attacking, tick up attack time and check if still attacking
-		_currentAttackTime = Mathf.Clamp(_currentAttackTime + Time.deltaTime, 0f,
-			_attackDuration);
-		//print(_swordPivotPoint.transform.eulerAngles);
-
-		if (_currentAttackTime == _attackDuration)
-		{
-			_attacking = false;
-		}
-		#endregion
+		//TESTING FOR GOOD ATTACKS.
+		SetAttackSpeeds();
 	}
 
 	#region Attack Methods
 	public void OnSlashInput(InputAction.CallbackContext context)
 	{
-		if (!context.performed || !_attackReady) return;
+		if (!context.performed || !_swingReady) return;
 		_swordAnimator.SetTrigger("Swing");
-		HandleBasicAttack(context);
+		SetAttackDirection();
+		StartCoroutine(BeginSwingCooldown());
 		return;
 	}
 
 	public void OnThrustInput(InputAction.CallbackContext context)
 	{	
-		if (!context.performed || !_attackReady) return;
+		if (!context.performed || !_thrustReady) return;
 		_swordAnimator.SetTrigger("Thrust");
-		HandleBasicAttack(context);
+		SetAttackDirection();
+		StartCoroutine(BeginThrustCooldown());
 		return;
 	}
 
 	public void OnSlamInput(InputAction.CallbackContext context)
 	{
-		if (!context.performed || !_attackReady) return;
+		if (!context.performed || !_slamReady) return;
 		_swordAnimator.SetTrigger("Slam");
-		HandleBasicAttack(context);
+		SetAttackDirection();
+		StartCoroutine(BeginSlamCooldown());
 		return;
 	}
 	#endregion
 
 	#region Helper Methods
-
-	/// <summary>
-	/// Sets up common elements between different basic attacks
-	/// like setting attack cooldowns and attack duration.
-	/// </summary>
-	private void HandleBasicAttack(InputAction.CallbackContext context)
+	private void SetAttackSpeeds()
 	{
-		//Reset attack cooldown and attack duration
-		_currentAttackCooldownTime = _attackCooldownDuration;
-		_attacking = true;
-		_currentAttackTime = 0f;
-
-		//Lock attack direction to cardinal/diagonal
-		//_mostRecentAttackDirection = GetAttackDirection();
-		if (GetAttackDirection(out Vector2 AttackDirection) == Vector2.left)
-		{
-			_swordSpriteRenderer.flipX = true;
-			_sword.transform.up = AttackDirection;
-		}
-		else
-		{
-			_swordSpriteRenderer.flipX = false;
-			_sword.transform.up = AttackDirection;
-		}
+		_swordAnimator.SetFloat(
+					"Swing Speed",
+					1 / (_swingAttackDuration / _swingAnimationClip.length));
+		_swordAnimator.SetFloat(
+			"Thrust Speed",
+			1 / (_thrustAttackDuration / _thrustAnimationClip.length));
+		_swordAnimator.SetFloat(
+			"Slam Speed",
+			1 / (_slamAttackDuration / _slamAnimationClip.length));
 	}
 
-	private Vector2 GetAttackDirection(out Vector2 realAttackDirection)
+	private IEnumerator BeginSwingCooldown()
 	{
+		_swingReady = false;
+		yield return new WaitForSeconds(_attackCooldownDuration);
+		_swingReady = true;
+	}
+
+	private IEnumerator BeginThrustCooldown()
+	{
+		_thrustReady = false;
+		yield return new WaitForSeconds(_attackCooldownDuration);
+		_thrustReady = true;
+	}
+
+	private IEnumerator BeginSlamCooldown()
+	{
+		_slamReady = false;
+		yield return new WaitForSeconds(_attackCooldownDuration);
+		_slamReady = true;
+	}
+
+	private void SetAttackDirection()
+	{
+		#region Calculate Attack Direction
 		Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		Vector2 rawAttackDirection = new Vector2(
 			mousePosition.x - _sword.transform.position.x,
 			mousePosition.y - _sword.transform.position.y);
 		float rawAngle = Vector2.SignedAngle(Vector2.right, rawAttackDirection);
+		Vector2 realAttackDirection;
 		//EAST
 		if (Mathf.Abs(rawAngle) < 22.5)
 		{
@@ -175,7 +161,13 @@ public class PlayerAttackScript : MonoBehaviour
 		{
 			realAttackDirection = Vector2.down + Vector2.right;
 		}
-		return realAttackDirection;
+		#endregion
+
+		//Flip if attack on left to prevent animation rotating.
+		if (realAttackDirection == Vector2.left) _swordSpriteRenderer.flipX = true;
+		else _swordSpriteRenderer.flipX = false;
+
+		_sword.transform.up = realAttackDirection;
 	}
 	#endregion
 }
