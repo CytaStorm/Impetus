a31480d33d44dbc2ae9c85535ac2f7c60b9553b2
Jeff <cytastorm@gmail.com>
2024-07-17 22:36:06 -0400

    made sword follow cursor

--- Assets/Scripts/Player/PlayerAttackScript.cs
+++ Assets/Scripts/Player/PlayerAttackScript.cs
@@ -8,9 +8,9 @@ public class PlayerAttackScript : MonoBehaviour
 {
 	#region GameObject Components
 	[Header("GameObject Components")]
-	[SerializeField] private GameObject _swordPivotPoint;
 	[SerializeField] private GameObject _sword;
 	private Animator _swordAnimator;
+	private SpriteRenderer _swordSpriteRenderer;
 	#endregion
 
 	#region Attack Properties
@@ -40,16 +40,14 @@ public class PlayerAttackScript : MonoBehaviour
 	{
 		get { return new Vector3(0, 0, _slashArc / 2); }
 	}
-	private Vector3 _slashArcBegin;
-	private Vector3 _slashArcEnd;
 
 	#endregion
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		_swordPivotPoint.SetActive(false);
 		_swordAnimator = _sword.GetComponent<Animator>();
+		_swordSpriteRenderer = _sword.GetComponent<SpriteRenderer>();
 	}
 
 	// Update is called once per frame
@@ -68,14 +66,11 @@ public class PlayerAttackScript : MonoBehaviour
 		//Is attacking, tick up attack time and check if still attacking
 		_currentAttackTime = Mathf.Clamp(_currentAttackTime + Time.deltaTime, 0f,
 			_attackDuration);
-		_swordPivotPoint.transform.eulerAngles = Vector3.Lerp(
-			_slashArcBegin, _slashArcEnd, _attackCompletion);
 		//print(_swordPivotPoint.transform.eulerAngles);
 
 		if (_currentAttackTime == _attackDuration)
 		{
 			_attacking = false;
-			_swordPivotPoint.SetActive(false);
 		}
 		#endregion
 	}
@@ -114,29 +109,32 @@ public class PlayerAttackScript : MonoBehaviour
 	/// </summary>
 	private void HandleBasicAttack(InputAction.CallbackContext context)
 	{
-		_swordPivotPoint.SetActive(true);
 		//Reset attack cooldown and attack duration
 		_currentAttackCooldownTime = _attackCooldownDuration;
 		_attacking = true;
 		_currentAttackTime = 0f;
 
 		//Lock attack direction to cardinal/diagonal
-		_mostRecentAttackDirection = GetAttackDirection();
-		_swordPivotPoint.transform.right = GetAttackDirection();
-		_slashArcBegin = _swordPivotPoint.transform.eulerAngles +
-			_slashArcOffset;
-		_slashArcEnd = _swordPivotPoint.transform.eulerAngles -
-			_slashArcOffset;
+		//_mostRecentAttackDirection = GetAttackDirection();
+		if (GetAttackDirection(out Vector2 AttackDirection) == Vector2.left)
+		{
+			_swordSpriteRenderer.flipX = true;
+			_sword.transform.up = AttackDirection;
+		}
+		else
+		{
+			_swordSpriteRenderer.flipX = false;
+			_sword.transform.up = AttackDirection;
+		}
 	}
 
-	private Vector2 GetAttackDirection()
+	private Vector2 GetAttackDirection(out Vector2 realAttackDirection)
 	{
 		Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		Vector2 rawAttackDirection = new Vector2(
-			mousePosition.x - _swordPivotPoint.transform.position.x,
-			mousePosition.y - _swordPivotPoint.transform.position.y);
+			mousePosition.x - _sword.transform.position.x,
+			mousePosition.y - _sword.transform.position.y);
 		float rawAngle = Vector2.SignedAngle(Vector2.right, rawAttackDirection);
-		Vector2 realAttackDirection;
 		//EAST
 		if (Mathf.Abs(rawAngle) < 22.5)
 		{
